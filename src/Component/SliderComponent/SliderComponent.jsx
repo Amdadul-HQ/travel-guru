2ad65b281839db0857686aa137558b4c7725fa0e
AmdadulHQ <rimonamdadul301@gmail.com>
2024-04-06 15:13:43 +0600

    adding slider images

--- src/Component/SliderComponent/SliderComponent.jsx
+++ src/Component/SliderComponent/SliderComponent.jsx
@@ -1,6 +1,10 @@
 import React, { useRef, useState } from 'react';
 import { Virtual, Navigation, Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import pic1 from '../../assets/Sajek.png'
+import pic2 from '../../assets/Sreemongol.png'
+import pic3 from '../../assets/sundorbon.png'
+
 
 // Import Swiper styles
 import 'swiper/css';
@@ -10,31 +14,33 @@ import 'swiper/css/navigation';
 import './Slider.css';
 
 export default function SliderComponent() {
+    const images = [pic1,pic2,pic3]
   const [swiperRef, setSwiperRef] = useState(null);
-  const appendNumber = useRef(500);
-  const prependNumber = useRef(1);
+//   const appendNumber = useRef(3);
+//   const prependNumber = useRef(1);
   // Create array with 500 slides
   const [slides, setSlides] = useState(
-    Array.from({ length: 500 }).map((_, index) => `Slide ${index + 1}`)
+    Array.from({ length: 3 }).map((_, index) => `Slide ${index + 1}`)
   );
 
-  const prepend = () => {
-    setSlides([
-      `Slide ${prependNumber.current - 2}`,
-      `Slide ${prependNumber.current - 1}`,
-      ...slides,
-    ]);
-    prependNumber.current = prependNumber.current - 2;
-    swiperRef.slideTo(swiperRef.activeIndex + 2, 0);
-  };
+//   const prepend = () => {
+//     setSlides([
+//       `Slide ${prependNumber.current - 2}`,
+//       `Slide ${prependNumber.current - 1}`,
+//       ...slides,
+//     ]);
+//     prependNumber.current = prependNumber.current - 2;
+//     swiperRef.slideTo(swiperRef.activeIndex + 2, 0);
+//   };
 
-  const append = () => {
-    setSlides([...slides, 'Slide ' + ++appendNumber.current]);
-  };
+//   const append = () => {
+//     setSlides([...slides, 'Slide ' + ++appendNumber.current]);
+//   };
 
-  const slideTo = (index) => {
-    swiperRef.slideTo(index - 1, 0);
-  };
+//   const slideTo = (index) => {
+//     swiperRef.slideTo(index - 1, 0);
+//   };
+
 
   return (
     <>
@@ -51,29 +57,11 @@ export default function SliderComponent() {
         virtual
       >
         {slides.map((slideContent, index) => (
-          <SwiperSlide key={slideContent} virtualIndex={index}>
-            {slideContent}
+          <SwiperSlide  key={slideContent} virtualIndex={index}>
+            <img  className='h-[410px]' src={images[index]} alt="" />
           </SwiperSlide>
         ))}
       </Swiper>
-
-      <p className="append-buttons">
-        <button onClick={() => prepend()} className="prepend-2-slides">
-          Prepend 2 Slides
-        </button>
-        <button onClick={() => slideTo(1)} className="prepend-slide">
-          Slide 1
-        </button>
-        <button onClick={() => slideTo(250)} className="slide-250">
-          Slide 250
-        </button>
-        <button onClick={() => slideTo(500)} className="slide-500">
-          Slide 500
-        </button>
-        <button onClick={() => append()} className="append-slides">
-          Append Slide
-        </button>
-      </p>
     </>
   );
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,14 +2,16 @@ import { FaArrowRight } from "react-icons/fa6";
 import SliderComponent from "../../Component/SliderComponent/SliderComponent";
 const Home = () => {
     return (
-        <section className="ml-36">
-            <div>
-                <h1 className="text-8xl font-bold text-white">COX'S BAZAR</h1>
-                <p className="text-base font-normal text-white max-w-[450px]">Cox's Bazar is a city, fishing port, tourism centre and district headquarters in southeastern Bangladesh. It is famous mostly for its long natural sandy beach, and it ...</p>
-                <button className="flex items-center gap-x-2 bg-btnBg font-medium text-base text-black py-3 px-7 rounded-lg">Booking<FaArrowRight/></button>
-            </div>
-            <div>
-                <SliderComponent></SliderComponent>
+        <section className="ml-36 flex items-center lg:h-[600px]">
+            <div className="flex items-center">
+                <div className="lg:w-[40%]">
+                    <h1 className="text-8xl font-bold text-white">COX'S BAZAR</h1>
+                    <p className="text-base font-normal text-white max-w-[450px]">Cox's Bazar is a city, fishing port, tourism centre and district headquarters in southeastern Bangladesh. It is famous mostly for its long natural sandy beach, and it ...</p>
+                    <button className="flex mt-6 items-center gap-x-2 bg-btnBg font-medium text-base text-black py-3 px-7 rounded-lg">Booking<FaArrowRight/></button>
+                </div>
+                <div className="overflow-hidden lg:w-[60%]">
+                    <SliderComponent></SliderComponent>
+                </div>
             </div>
         </section>
     );
